9802d424c9d8c146e5970be104be982db34c1193
Szymon Walter <walter.szymon.98@gmail.com>
2017-11-13 10:33:52 +0100

    Add array access methods

--- src/api/array.rs
+++ src/api/array.rs
@@ -1,9 +1,98 @@
 
+use std::slice;
+
 use sys::*;
+use error::Result;
+use api::{Value, JlValue};
 
 jlvalues! {
     pub struct Array(jl_array_t);
+    pub struct ByteArray(jl_array_t);
     pub struct Svec(jl_svec_t);
+}
+
+impl Array {
+    pub fn len(&self) -> Result<usize> {
+        let len = unsafe { jl_array_len(self.lock()?) };
+        Ok(len)
+    }
+
+    pub fn as_vec(&self) -> Result<Vec<Value>> {
+        let len = self.len()?;
+        let ptr = unsafe { jl_array_data(self.lock()?) as *mut *mut jl_value_t };
+        let slice = unsafe { slice::from_raw_parts(ptr, len) };
+        let vec = slice
+            .iter()
+            .map(|raw| unsafe { Value::new_unchecked(*raw) })
+            .collect();
+        Ok(vec)
+    }
+
+    pub fn index(&self, idx: usize) -> Result<Value> {
+        let raw = unsafe { jl_array_ptr_ref(self.lock()?, idx) };
+        Value::new(raw)
+    }
+
+    pub fn index_set(&self, idx: usize, x: &Value) -> Result<()> {
+        unsafe { jl_array_ptr_set(self.lock()?, idx, x.lock()?); }
+        Ok(())
+    }
+}
+
+impl ByteArray {
+    pub fn len(&self) -> Result<usize> {
+        let len = unsafe { jl_array_len(self.lock()?) };
+        Ok(len)
+    }
+
+    pub fn as_slice(&self) -> Result<&[u8]> {
+        let len = self.len()?;
+        let ptr = unsafe { jl_array_data(self.lock()?) as *mut u8 };
+        let slice = unsafe { slice::from_raw_parts(ptr, len) };
+        Ok((slice))
+    }
+
+    pub fn as_vec(&self) -> Result<Vec<u8>> {
+        self.as_slice().map(|s| s.to_vec())
+    }
+
+    pub fn index(&self, idx: usize) -> Result<u8> {
+        let byte = unsafe { jl_array_uint8_ref(self.lock()?, idx) };
+        Ok(byte)
+    }
+
+    pub fn index_set(&self, idx: usize, x: u8) -> Result<()> {
+        unsafe { jl_array_uint8_set(self.lock()?, idx, x); }
+        Ok(())
+    }
+}
+
+impl Svec {
+    pub fn len(&self) -> Result<usize> {
+        let len = unsafe { jl_svec_len(self.lock()?) };
+        Ok(len)
+    }
+
+    pub fn as_vec(&self) -> Result<Vec<Value>> {
+        let len = self.len()?;
+        let ptr = unsafe { jl_svec_data(self.lock()?) };
+        let slice = unsafe { slice::from_raw_parts(ptr, len) };
+        let vec = slice
+            .iter()
+            .map(|raw| unsafe { Value::new_unchecked(*raw) })
+            .collect();
+        Ok(vec)
+    }
+
+    pub fn index(&self, idx: usize) -> Result<Value> {
+        let raw = unsafe { jl_svecref(self.lock()?, idx) };
+        Value::new(raw)
+    }
+
+    pub fn index_set(&self, idx: usize, x: &Value) -> Result<()> {
+        unsafe { jl_svecset(self.lock()?, idx, x.lock()?); }
+        Ok(())
+    }
 }
 
 #[macro_export]
