26e6ab6d0609421b925cb5612b3baa12df603e5c
Szymon Walter <walter.szymon.98@gmail.com>
2017-11-15 19:59:57 +0100

    Replace old callback example
    The new example is tested and works.

--- examples/callback.rs
+++ examples/callback.rs
@@ -2,21 +2,36 @@
 #[macro_use]
 extern crate julia;
 
-use julia::api::{Julia, Float64};
+use julia::api::Julia;
+use julia::api::primitive::*;
 
 extern_jl! {
-    extern "Julia" libsquare :: Square {
+    extern "Julia" libsquare :: LibSquare {
         pub fn square(x: Float64) -> Float64 {
             x * x
         }
     }
 }
 
-fn main() {
-    let mut jl = Julia::new().unwrap();
+#[no_mangle]
+pub extern "C" fn libsquare_decl() {
+    // if Julia was created with Julia::new_unchecked, it won't trigger the
+    // at_exit hooks.
+    let mut jl = unsafe { Julia::new_unchecked() };
 
-    let sqr = Square::new();
+    let sqr = LibSquare::new();
     sqr.decl(&mut jl).unwrap();
-
-    jl.eval_string("square").unwrap();
 }
+
+// In Julia:
+/*
+
+dlname = "libsquare"
+push!(Libdl.DL_LOAD_PATH, "./") 
+libsquare = Libdl.dlopen(dlname)
+
+ccall((:square_decl, :libsquare), Void, ())
+
+println("square(4.0) = ", square(4.0))
+
+ */
